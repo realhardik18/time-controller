9b256b301f30a4ec0bdffca710cab536e2813e83
hardik singh <realhardik18@gmail.com>
2021-07-13 10:42:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import time
-print("hello realhardik18, enter the amount of time(minutes) your gonna do the task so we can remind you on time below")
+print("hello user, enter the amount of time(minutes) your gonna do the task so we can remind you on time below")
 user_input=input()
 timee=int(user_input)*60
 time.sleep(timee)
